9103fe74c398252c392df0d99e5e49cc6eefdcf0
Adrian Chifor <moonpidev@gmail.com>
2015-12-06 00:19:55 +0000

    Implemented /session API endpoint

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var multer = require('multer');
 var mongoose = require('mongoose');
+var uuid = require('node-uuid');
 var app = express();
 
 // Configure body parser
@@ -16,7 +17,10 @@ var Session = require('./app/models/session.js')
 var Participant = require('./app/models/participant.js')
 var Segment = require('./app/models/segment.js')
 
+var serverIp = "31.187.70.159";
 var port = 5000;
+
+var sessionCloseTasks = {};
 
 // Create our router
 var router = express.Router();
@@ -24,10 +28,89 @@ var router = express.Router();
 router.get('/session', function(req, res) {
 	var lat = req.param('lat');
 	var lng = req.param('lng');
+	var startedRecording = req.param('startedRecording');
 
-	res.statusCode = 200;
-	res.json({
-		key: 'abc'
+	var coords = [];
+	coords[0] = lng;
+	coords[1] = lat;
+
+ 	var maxDistance = 1;
+	maxDistance /= 6371;
+
+	Session.find({
+		location: {
+			$near: coords,
+			$maxDistance: maxDistance
+		}
+	}).where("active").equals(true)
+	.limit(1).exec(function(err, session) {
+		if (err) {
+			res.statusCode = 500;
+			res.json({
+				message: "Error"
+			});
+			return;
+		}
+
+		var newParticipant = new Participant();
+		newParticipant.startedRecording = startedRecording;
+		newParticipant.uploadUrl = "";
+		newParticipant.uploaded = false;
+
+		newParticipant.save(function(err) {
+			if (err) {
+				res.statusCode = 500;
+				res.json({
+					message: "Error"
+				});
+				return;
+			}
+
+			if (session.length == 0) {
+				session = new Session();
+				session.participantKeys = [];
+				session.participantKeys.push(newParticipant._id);
+				session.location = [lng, lat];
+				session.segments = [];
+				session.created = new Date().getTime();
+				session.finalUrl = uuid.v4() + ".mp4";
+				session.ready = false;
+				session.active = true;
+
+				session.save(function(err) {
+					if (err) {
+						res.statusCode = 500;
+						res.json({
+							message: "Error"
+						});
+						return;
+					}
+
+					res.statusCode = 200;
+					res.json({
+						key: newParticipant._id
+					});
+				});
+
+				return;
+			}
+
+			session[0].participantKeys.push(newParticipant._id);
+			session[0].save(function(err) {
+				if (err) {
+					res.statusCode = 500;
+					res.json({
+						message: "Error"
+					});
+					return;
+				}
+
+				res.statusCode = 200;
+				res.json({
+					key: newParticipant._id
+				});
+			});
+		});
 	});
 });
 
